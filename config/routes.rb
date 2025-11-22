850522d52f2d9b0e9b72f7179949c94fd311fff9
Michael Marchand <35391349+MarchandMD@users.noreply.github.com>
2025-11-22 10:18:09 -0700

    tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get "users/index"
+  get "tasks/index"
   devise_for :users
   get "up" => "rails/health#show", as: :rails_health_check
   resources :users
+  resources :tasks
   root "home#index"
 end
