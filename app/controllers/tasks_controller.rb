c2ff5836be80bba9f7a3ba96e6b12dd4b56f997b
Michael Marchand <35391349+MarchandMD@users.noreply.github.com>
2025-11-22 14:32:12 -0700

    implement task creation and user management features; add new task form, update user index view, and enhance layout with user menu

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
   def index
     @tasks = Task.all
 
@@ -11,5 +13,28 @@ class TasksController < ApplicationController
 
   def show
     @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = current_user.tasks.build(task_params)
+    @task.status ||= "open"
+    @task.upvotes_count ||= 0
+    @task.submissions_count ||= 0
+
+    if @task.save
+      redirect_to task_path(@task), notice: "Task created successfully!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :description, :status)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
   def index
+    @users = User.order(created_at: :desc)
+    @groups = User.distinct.pluck(:group).compact.sort
   end
 end
