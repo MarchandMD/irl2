aa055d9cceb5a9d88496c82e4df466a0104aeb02
Michael Marchand <35391349+MarchandMD@users.noreply.github.com>
2026-03-01 09:13:34 -0700

    default admin to false

--- db/schema.rb
+++ db/schema.rb
@@ -268,7 +268,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_01_160002) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.boolean "admin"
+    t.boolean "admin", default: false, null: false
     t.datetime "archived_at"
     t.datetime "created_at", null: false
     t.string "email", default: "", null: false
